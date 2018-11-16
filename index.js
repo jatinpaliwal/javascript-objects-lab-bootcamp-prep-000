7ca2a7a6726e954fe72fc4d5a287b5491a044240
Jatin Paliwal <jatinpaliwal181@gmail.com>
2018-11-16 19:02:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes,key,value){
   recipes[key]=value
   return recipes
 }
+
+function deleteFromObjectByKey(recipes,key){
+  var newObj = Object.assign({},recipes)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(recipes,key){
+  delete recipes[key]
+  return recipes
+}
